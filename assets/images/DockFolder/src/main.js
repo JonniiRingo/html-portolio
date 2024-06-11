c72678875b1ce03460ab8b9dbd969a0840cee81d
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-06-10 19:13:25 -0700

    Group assets together

--- assets/images/DockFolder/src/main.js
+++ assets/images/DockFolder/src/main.js
@@ -3,3 +3,5 @@ import { Application } from '@splinetool/runtime';
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/SN3CRVWfyhcuf2Hg/scene.splinecode');
+
+
